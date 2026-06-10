40d6e122400fba31b14390cc8ab1195c5b7b695d
NiccoAndreozzi <niccoloandreozzi1@gmail.com>
2026-06-10 11:10:13 +0200

    Aggiunta Personaggio nemico in develop 2

--- src/main/java/it.rjcsoft.dungeonbattle.personaggi/Nemico.java
+++ src/main/java/it.rjcsoft.dungeonbattle.personaggi/Nemico.java
@@ -2,25 +2,13 @@ package it.rjcsoft.dungeonbattle.personaggi;
 
 public abstract class Nemico extends Personaggio
 {
-    private int stamina;
-    private int stamina_gain;
+
     public Nemico(String nome,int max_health,int attacco_base, int stamina_gain)
     {
         super(nome,max_health,attacco_base);
-        this.stamina = 0;
-        this.stamina_gain = stamina_gain;
-    }
 
-    public int getStamina()
-    {
-       return this.stamina;
     }
 
 
-
-    public void setStamina(int stamina)
-    {
-        this.stamina = stamina;
-    }
 
 }
