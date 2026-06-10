228ce197e6488b303cfc658e745cd65098a09595
ambra-bae <ambra.baesso08@gmail.com>
2026-06-10 09:36:39 +0200

    Pacchetto nemici con goblin orco scheletro

---
+++ src/main/java/it/rjcsoft/dangeounbattle/nemici/Goblin.java
@@ -0,0 +1,11 @@
+package it.rjcsoft.dangeounbattle.nemici;
+
+import it.rjcsoft.dungeonbattle.personaggi.Nemico;
+
+public class Goblin extends Nemico
+{
+    public Goblin (String nome,int max_health,int attacco_base){
+        super("GOBLIN", 100,15);
+    }
+
+}
---
+++ src/main/java/it/rjcsoft/dangeounbattle/nemici/Orco.java
@@ -0,0 +1,10 @@
+package it.rjcsoft.dangeounbattle.nemici;
+
+import it.rjcsoft.dungeonbattle.personaggi.Nemico;
+
+public class Goblin extends Nemico
+{
+    public Goblin (String nome,int max_health,int attacco_base){
+        super("ORCO", 150,30);
+    }
+}
