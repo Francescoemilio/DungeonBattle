4ddf57b3cd2580682b56c435d7b2a8ce05dd30e3
Filippo paperini <filippopaperini@syn-172-254-010-066.biz.spectrum.com>
2026-06-11 14:51:28 +0200

    IOException

--- src/main/java/it/rjcsoft/dungeonbattle/dungeon/Dungeon.java
+++ src/main/java/it/rjcsoft/dungeonbattle/dungeon/Dungeon.java
@@ -1,5 +1,6 @@
 package it.rjcsoft.dungeonbattle.dungeon;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
@@ -45,7 +46,7 @@ public class Dungeon {
         }
     }
 
-    public void start(){
+    public void start()throws IOException{
         if (this.eroe == null) {
             System.out.println("se");
             this.eroe = new Eroe("Francesco Barsotti", 200, 20, new Arma(0), 3);
@@ -61,10 +62,8 @@ public class Dungeon {
         System.out.println("2. Medio");
         System.out.println("3. Difficile");
         System.out.println("Altro per terminare il gioco");
-
-        int difficolta = input.nextInt();//non vi serve la variabile
         int quantita;
-        switch (difficolta) {
+        switch (input.nextInt()) {
             case 1:
                 quantita = 3;
                 System.out.println(" ");
@@ -122,8 +121,7 @@ public class Dungeon {
         System.out.println(" ");
         System.out.println("1. Si");
         System.out.println("2. No");
-        int newP = input.nextInt();
-        switch (newP){
+        switch (input.nextInt()){
             case 1:
                 System.out.println("Ripartiamo!");
                 System.out.println(" ");
