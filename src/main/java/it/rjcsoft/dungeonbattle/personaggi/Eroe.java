2c2b29b8b147ac008cd25cf112007e7aedc44311
NiccoAndreozzi <niccoloandreozzi1@gmail.com>
2026-06-11 16:49:25 +0200

    Correzione costruttore eroe

--- src/main/java/it/rjcsoft/dungeonbattle/personaggi/Eroe.java
+++ src/main/java/it/rjcsoft/dungeonbattle/personaggi/Eroe.java
@@ -10,8 +10,8 @@ public class Eroe extends Personaggio {
 	private static final int MAX_VITA = 250;
 	private static final int ATTACCO_BASE = 20;
 
-	public Eroe(String nome, int maxHealth, int attaccoBase, Oggetto arma, int nPozioni) {
-		super(nome, maxHealth, attaccoBase); 
+	public Eroe(String nome,  Oggetto arma, int nPozioni) {
+		super(nome ,MAX_VITA,ATTACCO_BASE);
 		oggetto = arma;
 		o = new ArrayList<Oggetto>(nPozioni);
 		System.out.println(o.size() + "    " + nPozioni);
