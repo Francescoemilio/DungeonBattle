71802d30a6d51a6cdacb25a14cabd0a7ebd3efc9
Marco Cauteruccio <gcauteruccio0@gmail.com>
2026-06-11 16:27:21 +0200

    Buff scheletro

--- src/main/java/it/rjcsoft/dungeonbattle/nemici/Scheletro.java
+++ src/main/java/it/rjcsoft/dungeonbattle/nemici/Scheletro.java
@@ -3,8 +3,8 @@ package it.rjcsoft.dungeonbattle.nemici;
 import it.rjcsoft.dungeonbattle.personaggi.Nemico;
 
 public class Scheletro extends Nemico {
-    private static final int MAX_VITA = 50;
-    private static final int ATTACCO_BASE = 10;
+    private static final int MAX_VITA = 100;
+    private static final int ATTACCO_BASE = 15;
 
     public Scheletro(String nome) {
         super("SCHELETRO", MAX_VITA, ATTACCO_BASE);
